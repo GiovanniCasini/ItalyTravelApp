de770ff17641c8fc659f84485a0cbdd2c18ab4fa
Agata Parietti <agata.parietti@stud.unifi.it>
2024-01-05 22:00:40 +0100

    Add files via upload

--- get_ontology.py
+++ get_ontology.py
@@ -7,11 +7,21 @@ g.parse("ItalyTravelApp_ontology.rdf", format="xml")  # Assicurati di specificar
 
 # Definisci la query SPARQL per ottenere gli individui della classe Destination
 query_string = """
-SELECT ?destination
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX myOnt: <http://www.semanticweb.org/agata/ontologies/2023/11/ItalyTravelApp_Ontology#>
+
+SELECT (strafter(str(?individual), "#") as ?individualName)
 WHERE {
-   ?destination a <http://www.w3.org/2002/07/owl#Class>.
+   ?subclass rdf:type owl:Class ;
+             rdfs:subClassOf* myOnt:Destination .
+   
+   ?individual rdf:type ?subclass .
 }
+
 """
+
 
 # Prepara la query
 query = prepareQuery(query_string)
@@ -19,4 +29,4 @@ query = prepareQuery(query_string)
 # Esegui la query e stampa i risultati
 results = g.query(query)
 for row in results:
-   print(row.destination)
+   print(row.individualName)
