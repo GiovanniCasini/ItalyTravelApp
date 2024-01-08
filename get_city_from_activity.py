489efb9e0c61713464e3bfac8b9ba1aced839cc8
Agata Parietti <agata.parietti@stud.unifi.it>
2024-01-08 18:08:13 +0100

    Add files via upload

---
+++ get_city_from_activity.py
@@ -0,0 +1,30 @@
+from rdflib import Graph, Literal
+from rdflib.plugins.sparql import prepareQuery
+
+# Carica il grafo RDF dal file
+g = Graph()
+g.parse("ItalyTravelApp_ontology.rdf", format="xml")  # Assicurati di specificare il formato corretto
+
+# Definisci la query SPARQL per ottenere gli individui della classe Destination
+query_string = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX myOnt: <http://www.semanticweb.org/agata/ontologies/2023/11/ItalyTravelApp_Ontology#>
+
+SELECT DISTINCT (strafter(str(?city), "#") as ?cityName)
+WHERE {
+    ?city rdf:type myOnt:City .
+    ?city myOnt:hasActivity ?activity .
+    ?activity rdf:type myOnt:Museums .
+}
+"""
+
+
+# Prepara la query
+query = prepareQuery(query_string)
+
+# Esegui la query e stampa i risultati
+results = g.query(query)
+for row in results:
+    print(row.cityName)
