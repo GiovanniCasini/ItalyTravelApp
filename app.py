b33d55047036209d9700014696ba5dd49b537070
Giovanni Casini <giovanni.casini98@gmail.com>
2024-01-09 16:57:44 +0100

    comments

--- app.py
+++ app.py
@@ -3,21 +3,17 @@ import dash
 from dash import dcc, html, Input, Output, State
 from get_ontology import get_destinations, get_activities, get_class_activities, get_city_from_activity
 
-# Lista di città italiane
+# get lists from ontology
 destinations = get_destinations()
 activities_list = get_class_activities()
 
-# Inizializza l'applicazione Flask
+# initialization with Flask and Dash
 app_flask = Flask(__name__, static_url_path='/static')
-
-# Inizializza l'applicazione Dash e connettila all'app Flask
 app_dash = dash.Dash(__name__, server=app_flask)
 
-# Funzione per ottenere le attività di una città
 def get_activities_for_city(dest):
     return get_activities(dest)
 
-# Definisci la layout della dashboard Dash
 def generate_city_button(city, index):
     return html.Button(
         city,
@@ -34,36 +30,29 @@ def generate_city_button(city, index):
         }
     )
 
-# Applica uno sfondo al layout direttamente in Python
+# layout of the web app
 app_dash.layout = html.Div(children=[
     html.H1("ITALY TRAVEL PLANNER", style={'textAlign': 'center','margin-top': '2%','font-family': 'Arial, sans-serif', 'font-size': '50px'}),
     html.H2("Choose your destination", style={'textAlign': 'center','font-family': 'Arial','margin-top': '5%'}),
-    
-    # Applica lo stile di sfondo al div del pulsante delle città
+    # generate destination buttons
     html.Div([generate_city_button(city, idx) for idx, city in enumerate(destinations)],
              className='button-container', style={'text-align': 'center', 'background': 'linear-gradient(to right, #001F3F, #001C2E)'}),
     
     html.H2("...or choose which activity you would like to do in Italy", style={'textAlign': 'center','font-family': 'Arial','margin-top': '5%'}),
-    
-    # Applica lo stile di sfondo al div delle attività
+    # generate activity buttons
     html.Div([generate_city_button(city, idx+len(destinations)) for idx, city in enumerate(activities_list)],
              className='button-container', style={'text-align': 'center', 'background': 'linear-gradient(to right, #001F3F, #001C2E)'}),
 
-    # Div contenitore per attività e immagine
     html.Div([
-        # Div per attività
         html.Div(id='activity-output', style={'width': '50%', 'display': 'inline-block', 'font-size': '20px',
                                                'margin-left': '5%', 'margin-top': '5%', 'vertical-align': 'top'}),
-        
-        # Div per l'immagine
         html.Div(id='destination-image', style={'width': '40%', 'display': 'inline-block', 'margin-right': '5%', 'margin-top': '5%'}),
     ]),
 
-    # Store per il bottone selezionato
     dcc.Store(id='selected-button', data=0)
 ])
 
-# Modifica il callback per centrare l'output delle attività
+# handle the updates after clicking on buttons
 @app_dash.callback(
     [Output('activity-output', 'children'),
      Output({'type': 'button', 'index': dash.ALL}, 'style'),
@@ -73,19 +62,17 @@ app_dash.layout = html.Div(children=[
      State('selected-button', 'data')]
 )
 def update_activities(selected_city_clicks, current_output, selected_button):
-    # Trova l'indice del pulsante cliccato
+    # find the index of the selected button
     ctx = dash.callback_context
     clicked_button_index = int(ctx.triggered_id['index'])
 
-    # Handle the case when the clicked button is related to an activity
+    # handle the case when the clicked button is related to an activity
     if clicked_button_index >= len(destinations):
         activity_index = clicked_button_index - len(destinations)
         destination = str(activities_list[activity_index])
-        # Adjust the index for activities
         activities = get_city_from_activity(destination.replace(" ", "_"))
-        # Adjust the clicked_button_index for styling
         clicked_button_index = activity_index + len(destinations)
-        # Aggiorna l'output con le attività della città selezionata, centrato
+        # update the output with cities where you can do the selected activity
         updated_output = [
             html.H1(f'{destination}', style={'color': 'red'}),
             html.H2(f'Explore {destination} - Information:', style={'marginTop': '20px'}),
@@ -93,11 +80,9 @@ def update_activities(selected_city_clicks, current_output, selected_button):
             html.Ul([html.Li(activity) for activity in activities]),
         ]
     else:
-        # The clicked button corresponds to a destination
         destination = str(destinations[clicked_button_index])
-        # Get activities for the selected destination
         activities, is_similar_to = get_activities_for_city(destination.replace(" ", "_"))
-        # Aggiorna l'output con le attività della città selezionata, centrato
+        # update the output with the activities of the selected destination
         updated_output = [
             html.H1(f'{destination}', style={'color': 'red'}),
             html.H2(f'Explore {destination} - Information:', style={'marginTop': '20px'}),
@@ -106,10 +91,7 @@ def update_activities(selected_city_clicks, current_output, selected_button):
             html.H2(f"Consider visiting {is_similar_to}, a destination similar to {destination}."),
         ]
 
-    # Aggiorna l'immagine della destinazione
     destination_image = html.Img(src=f'/static/images/{destination.lower().replace(" ", "_")}.jpg', style={'width': '100%'})
-
-    # Aggiorna lo stile dei pulsanti
     updated_button_style = [{'width': '120px', 'height': '120px', 'background-color': 'lightblue',
                              'margin': '10px', 'fontSize': '16px', 'fontWeight': 'bold'}] * len(destinations + activities_list)
     updated_button_style[clicked_button_index] = {'width': '120px', 'height': '120px', 'background-color': 'lightgreen',
@@ -117,6 +99,5 @@ def update_activities(selected_city_clicks, current_output, selected_button):
 
     return updated_output, updated_button_style, destination_image
 
-# Aggiungi questa riga dopo la definizione di `app_dash`
 if __name__ == '__main__':
     app_flask.run(debug=True)
